6141a7e37425900a8878b3171bfb001f31e4d478
Ali <mohammadali18997@gmail.com>
2020-10-23 18:13:24 -0400

    Imports and Starter

--- Pastry/Pastry/Program.fs
+++ Pastry/Pastry/Program.fs
@@ -1,8 +1,23 @@
-﻿// Learn more about F# at http://fsharp.org
+﻿open Akka
+open Akka.FSharp
+open System
+open System.Diagnostics
+open Akka.Actor
+
 
 open System
 
+//Create System reference
+let system = System.create "system" <| Configuration.defaultConfig()
+
+
+let mutable numNodes = 0
+let mutable numRequests = 0
+
 [<EntryPoint>]
-let main argv =
-    printfn "Hello World from F#!"
+let main (args) =
+    numNodes <- args.[0] |> int
+    numRequests <- args.[1] |> int
+
+
     0 // return an integer exit code
